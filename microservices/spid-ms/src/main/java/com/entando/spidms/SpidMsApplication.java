6186e5a11db2cc0d41934852bd638e629230cbc8
eglantina <metani.egla@gmail.com>
2023-05-15 09:23:00 +0200

    inizializzazione progetto

---
+++ microservices/spid-ms/src/main/java/com/entando/spidms/SpidMsApplication.java
@@ -0,0 +1,13 @@
+package com.entando.example.spidms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpidMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpidMsApplication.class, args);
+	}
+
+}
---
+++ microservices/spid-ms/src/test/java/com/entando/example/spidms/SpidMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.entando.example.spidms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpidMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
